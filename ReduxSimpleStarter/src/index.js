b6c158bc03ea29f5e2662ad2548fda2fd159f4ff
rikmms <sousa.ricardo10@gmail.com>
2017-09-02 20:52:35 +0100

    Finish the first section - An Intro to React (Components, Functional Components, Classe components for manage state and add more functionality).

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
 
-const App = () => <div>Hi!!!</div>
+const API_KEY = 'AIzaSyBVAwOIoWBPTBx8dyiE3SD4JVGPeaNrEQ4'
+
+const App = () => (<div>
+  <SearchBar />
+</div>)
 
 // Is necessary to pass a component's instance! 
 // JSX creates component instances (see in the Babel's online repl)
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('#app'))
