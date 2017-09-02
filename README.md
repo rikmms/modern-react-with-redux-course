e4ba3ab9b12223ca52405125818caf30eec7e3a2
rikmms <sousa.ricardo10@gmail.com>
2017-09-03 00:40:42 +0100

    End section 2 (props).

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Modern React with Redux Course Udemy Course
+# Modern React with Redux - Udemy Course
 
 This repository contains all the source code that I implemented during the course.
 
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyBVAwOIoWBPTBx8dyiE3SD4JVGPeaNrEQ4'
 
-const App = () => (<div>
-  <SearchBar />
-</div>)
+class App extends Component{
 
-// Is necessary to pass a component's instance! 
-// JSX creates component instances (see in the Babel's online repl)
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    }
+
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, 
+      videos => this.setState({ videos, selectedVideo: videos[0] })
+    )
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={ videoSearch } />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList 
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+          videos={ this.state.videos } />
+      </div>
+    )
+  }
+} 
+
 ReactDOM.render(<App />, document.querySelector('#app'))
